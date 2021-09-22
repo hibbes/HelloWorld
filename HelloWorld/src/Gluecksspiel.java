0d7833417ea9ad65ffa3495e002ab69097ddaf9a
MarekCzernohous <MarekCzernohous@ALZi-Marek>
2021-09-22 11:24:21 +0200

    OOP-Refactor

--- HelloWorld/src/Gluecksspiel.java
+++ HelloWorld/src/Gluecksspiel.java
@@ -1,41 +1,71 @@
 public class Gluecksspiel {
 
-	static int zufallszahl = (int) Math.round(Math.random() * 100);
-	static byte alter = 19;
-	static byte geschlecht = 2;
-	static String name = "Marek Czernohous";
-	static int spieler1Zahl = 42;
-	static int spieler2Zahl = 87;
-	static int diff1 = 0;
-	static int diff2 = 0;
+
+	
 
 	public static void main(String[] args) {
-		System.out.println("Zufallszahl= " + zufallszahl);
-		System.out.println("Spieler 1= " + spieler1Zahl);
-		System.out.println("Spieler 2= " + spieler2Zahl);
+	
+		Spieler s1 = new Spieler(16, 1, "Luisa Huber", 42);
+		Spieler s2 = new Spieler(20
+				
+				, 0, "Tom Schmieder", 87);
+		
+		System.out.println(Gluecksspiel(s1, s2));
+		
+		
+	}
 
-		diff1 = Math.abs(zufallszahl - spieler1Zahl);
-		diff2 = Math.abs(zufallszahl - spieler2Zahl);
+	public static String Gluecksspiel(Spieler spieler1, Spieler spieler2) {
+		int zufallszahl = (int) Math.round(Math.random() * 100);
+		String ausgabe = "";
+		int diff1 = 0;
+		int diff2 = 0;
+		
+		ausgabe = ausgabe+begruessung(spieler1, spieler2);
+		
+		ausgabe = ausgabe+"Zufallszahl= " + zufallszahl+"\n";
+		ausgabe = ausgabe+spieler1.name+" : " + spieler1.ratezahl+"\n";
+		ausgabe = ausgabe+spieler2.name+" : " + spieler2.ratezahl+"\n";
+		
+		diff1 = Math.abs(zufallszahl - spieler1.ratezahl);
+		diff2 = Math.abs(zufallszahl - spieler2.ratezahl);
 
 		if (diff1 == diff2) {
 
-			System.out.println("Unentschieden!");
+			ausgabe = ausgabe +"Unentschieden!";
 		} else {
 			if (diff1 < diff2) {
-				System.out.println("Spieler 1 gewinnt!");
+				ausgabe = ausgabe + spieler1.name+" gewinnt!";
 			} else {
-				System.out.println("Spieler 2 gewinnt!");
+				ausgabe = ausgabe + spieler2.name+" gewinnt!";
+			}
+		}
+		
+		return ausgabe;
+	}
+
+	public static String begruessung(Spieler spieler1, Spieler spieler2) {
+		String begruessung = "";
+		if (spieler1.alter < 18 || spieler1.geschlecht == 2) {
+			begruessung = "Hallo " + spieler1.name + ",\n";
+		} else {
+			if (spieler1.geschlecht == 1) {
+				begruessung = "Guten Tag Frau " + spieler1.alter + ",\n";
+			} else {
+				begruessung = "Guten Tag Herr " + spieler1.alter + ",\n";
+			}
+		}
+		if (spieler2.alter < 18 || spieler2.geschlecht == 2) {
+			begruessung = begruessung + "Hallo " + spieler2.name + ".\n";
+		} else {
+			if (spieler2.geschlecht == 1) {
+				begruessung = begruessung + "Guten Tag Frau " + spieler2.name + ".\n";
+			} else {
+				begruessung = begruessung + "Guten Tag Herr " + spieler2.name + ".\n";
 			}
 		}
 
-		if (alter < 18 || geschlecht == 2) {
-			System.out.println("Hallo " + name + "!");
-		} else {
-			if (geschlecht == 1) {
-				System.out.println("Guten Tag Frau " + name + "!");
-			} else {
-				System.out.println("Guten Tag Herr " + name + "!");
-			}
-		}
+		return begruessung;
 	}
+
 }
---
+++ HelloWorld/src/Spieler.java
@@ -0,0 +1,19 @@
+
+public class Spieler {
+
+	int alter = 19;
+	int geschlecht = 2;
+	String name = "Marek Czernohous";
+	int ratezahl = 0;
+	
+	
+	
+	public Spieler(int alter, int geschlecht, String name, int ratezahl) {
+		this.alter=alter;
+		this.geschlecht=geschlecht;
+		this.name=name;
+		this.ratezahl=ratezahl;
+		
+	}
+	
+}
